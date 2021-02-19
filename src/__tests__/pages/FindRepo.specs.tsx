1095430437e2876622a559f74707cc0a040b5688
piagja <piagja@gmail.com>
2021-02-19 13:35:23 -0300

    Realizou primeiro teste unitário

---
+++ src/__tests__/pages/FindRepo.specs.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from "../../pages/home"
+import { render } from '@testing-library/react'
+
+
+describe('Home App', () => {
+  it('Find repo', () => {
+    const {} = render(<Home/>)
+  })
+})
